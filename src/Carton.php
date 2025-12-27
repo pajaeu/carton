c298730ecbf3b2947cf66f2637348d78df2c7956
Pavel Skrbel <fgocsgo@gmail.com>
2025-12-27 19:23:31 +0100

    feat: add base cart methods

--- src/Carton.php
+++ src/Carton.php
@@ -1,5 +1,55 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Carton\Carton;
 
-class Carton {}
+use Carton\Carton\Models\Cart;
+
+final class Carton
+{
+    private const string SESSION_KEY = '_carton_cart_id';
+
+    private ?Cart $cart = null;
+
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    public function init(): void
+    {
+        $user = auth()->user();
+
+        if ($user) {
+            $this->cart = Cart::query()
+                ->where('is_active', true)
+                ->where('user_id', $user->id)
+                ->first();
+
+            return;
+        }
+
+        if (session()->has(self::SESSION_KEY)) {
+            $this->cart = Cart::query()
+                ->where('is_active', true)
+                ->find(session(self::SESSION_KEY));
+        }
+    }
+
+    public function getCart(): ?Cart
+    {
+        return $this->cart;
+    }
+
+    public function setCart(Cart $cart): void
+    {
+        $this->cart = $cart;
+
+        if ($this->cart->user) {
+            return;
+        }
+
+        session()->put(self::SESSION_KEY, $this->cart->id);
+    }
+}
