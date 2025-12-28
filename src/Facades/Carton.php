ff1014e9e1f54b9559ecfef46d8c54c1709d9a0b
Pavel Skrbel <fgocsgo@gmail.com>
2025-12-28 20:38:28 +0100

    feat: add method annotations to facade

--- src/Facades/Carton.php
+++ src/Facades/Carton.php
@@ -4,9 +4,28 @@ declare(strict_types=1);
 
 namespace Carton\Carton\Facades;
 
+use Carton\Carton\Data\CartLineData;
+use Carton\Carton\Models\Cart;
+use Carton\Carton\Models\CartLine;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void init()
+ * @method static Cart|null getCart()
+ * @method static void setCart(Cart $cart)
+ * @method static Collection<int, CartLine> getCartLines()
+ * @method static float getCartSubtotal(bool $withVat = true)
+ * @method static float getCartTotal(bool $withVat = true)
+ * @method static string getCartCurrencyCode()
+ * @method static Cart createCart(?string $currencyCode = null)
+ * @method static void destroyCart(Cart $cart)
+ * @method static void mergeUserCart(Authenticatable $user)
+ * @method static CartLine addLine(CartLineData $data, int $quantity = 1)
+ * @method static bool updateLineQuantity(CartLine $line, int $quantity)
+ * @method static void recalculate()
+ *
  * @see \Carton\Carton\Carton
  */
 final class Carton extends Facade
